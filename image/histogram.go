82c73ce9831f5ab72e871fb0e0ed10633e8baf2d
mushoffa <ridwan.mushoffa@gmail.com>
2024-07-13 20:46:10 +0700

    fix(histogram): Fix vertical axis orientation

--- image/histogram.go
+++ image/histogram.go
@@ -5,23 +5,29 @@ package image
 // vertical = true, vertical axis profile
 func (p *Monochrome) Histogram(vertical bool) []uint16 {
 	bounds := p.Bounds()
+	
 	minRow := bounds.Min.Y
 	maxRow := bounds.Max.Y
 	minColumn := bounds.Min.X
 	maxColumn := bounds.Max.X
 
 	if vertical {
-		minRow = bounds.Min.X
-		maxRow = bounds.Max.X
-		minColumn = bounds.Min.Y
-		maxColumn = bounds.Max.Y
+		minRow, minColumn = minColumn, minRow
+		maxRow, maxColumn = maxColumn, maxRow
 	}
 
 	histogram := make([]uint16, maxRow)
 
 	for row := minRow; row < maxRow; row++ {
 		for column := minColumn; column < maxColumn; column++ {
-			pixel := p.At(column, row).(MonochromeColor).Y
+			c := column
+			r := row
+
+			if vertical {
+				c, r = r, c
+			}
+
+			pixel := p.At(c, r).(MonochromeColor).Y
 			// 8-bit grayscale color
 			// 0 = black
 			// 255 = white
