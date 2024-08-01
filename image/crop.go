7dc2fabaeb35df7e01249fe5e7132da8d0ebccf1
mushoffa <ridwan.mushoffa@gmail.com>
2024-08-01 22:08:31 +0700

    image: Add crop function for Image and Monochrome type

---
+++ image/crop.go
@@ -0,0 +1,22 @@
+package image
+
+import (
+	img "image"
+)
+
+type CropRectangle interface {
+	SubImage(r img.Rectangle) img.Image
+}
+
+func (p *Image) Crop(minX, minY, maxX, maxY int) img.Image {
+	r := img.Rect(minX, minY, maxX, maxY)
+	cropped := p.data.(CropRectangle).SubImage(r)
+
+	return cropped
+}
+
+func (p *Monochrome) Crop(minX, minY, maxX, maxY int) img.Image {
+	cropped := p.SubImage(img.Rect(minX, minY, maxX, maxY))
+
+	return cropped
+}
