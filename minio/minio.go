6cafc914051add5ce90e87baf7435965077ae3bd
mushoffa <ridwan.mushoffa@gmail.com>
2024-08-06 18:53:20 +0700

    minio(ListFolder): Fix previous commit on spliting prefix name

--- minio/minio.go
+++ minio/minio.go
@@ -5,7 +5,6 @@ import (
 	"context"
 	"encoding/json"
 	"io"
-	"strings"
 
 	"github.com/minio/minio-go/v7"
 	"github.com/minio/minio-go/v7/pkg/credentials"
@@ -15,9 +14,9 @@ import (
 type MinioService interface {
 	GetInstance() *minio.Client
 	CreateBucket(string, string) error
-	ListBuckets() ([]minio.BucketInfo, error)
+	ListBucket() ([]minio.BucketInfo, error)
 	DeleteBucket(string) error
-	ListFolders(string, ...string) (string, error)
+	ListFolder(string, ...string) (string, error)
 	PutObject(string, string, []byte, ...string) error
 	GetObject(string, string, ...string) ([]byte, error)
 }
@@ -25,6 +24,13 @@ type MinioService interface {
 // client implements [MinioService] interface.
 type client struct {
 	instance *minio.Client
+}
+
+// file
+type file struct {
+	Id        string `json:"id,omitempty"`
+	Name      string `json:"name"`
+	Timestamp string `json:"timestamp"`
 }
 
 // NewClient returns a new MinIO client which exposes [MinioService] methods.
@@ -51,7 +57,7 @@ func (c *client) CreateBucket(bucketName, region string) error {
 }
 
 // ListBuckets returns a list of buckets created previously.
-func (c *client) ListBuckets() ([]minio.BucketInfo, error) {
+func (c *client) ListBucket() ([]minio.BucketInfo, error) {
 	return c.instance.ListBuckets(context.Background())
 }
 
@@ -61,13 +67,12 @@ func (c *client) DeleteBucket(bucketName string) error {
 }
 
 // ListFolders returns a list of foler path and files in a bucket.
-func (c *client) ListFolders(bucketName string, paths ...string) (string, error) {
+func (c *client) ListFolder(bucketName string, paths ...string) (string, error) {
 	ctx, cancel := context.WithCancel(context.Background())
 	defer cancel()
 
 	folderPath := c.getPath(paths...)
-
-	var payload []map[string]string
+	var files []file
 
 	objects := c.listObjects(ctx, bucketName, minio.ListObjectsOptions{Prefix: folderPath})
 
@@ -76,18 +81,16 @@ func (c *client) ListFolders(bucketName string, paths ...string) (string, error)
 			return "", object.Err
 		}
 
-		name := strings.Split(object.Key, "/")
-
-		obj := map[string]string{
-			"id":        object.ETag,
-			"name":      name[1],
-			"timestamp": object.LastModified.String(),
+		f := file{
+			Id:        object.ETag,
+			Name:      object.Key,
+			Timestamp: object.LastModified.String(),
 		}
 
-		payload = append(payload, obj)
+		files = append(files, f)
 	}
 
-	j, err := json.Marshal(payload)
+	j, err := json.Marshal(files)
 	if err != nil {
 		return "", err
 	}
